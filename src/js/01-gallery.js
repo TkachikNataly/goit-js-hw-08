1ab4a11faf7737accff67837dd2e5dab85a68580
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-30 00:37:20 +0200

    push task -01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,36 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+
+import SimpleLightbox from "simplelightbox";
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import '../css/common.css';
 import '../css/01-gallery.css';
 
 // Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryContainerRef = document.querySelector('.gallery');
+const imagesMarkup = galleryItems.map(({ preview, original, description }) => {
+       return `
+<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+}).join('');
+  
+galleryContainerRef.insertAdjacentHTML("beforeend", imagesMarkup);
 
-const galleryContainerEl = document.querySelector('.gallery');
-
-const galleryItemsList = makeGalleryItemsListMarkup(galleryItems);
-galleryContainerEl.insertAdjacentHTML('beforeend', galleryItemsList);
-
-function makeGalleryItemsListMarkup(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `
-      <li>
-      <a class="gallery__item" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        alt= "${description}"
-      />
-      </a>
-      </li>    
-    `;
-    })
-    .join('');
-}
-
-let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+    let gallery = new SimpleLightbox('.gallery a',
+        {
+            captionsData: 'alt',
+            captionDelay: 250,
+        });
+console.log(gallery);
