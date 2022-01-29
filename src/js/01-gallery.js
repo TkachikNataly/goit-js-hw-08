95eec7611abb56a20419f2f1a4fa1c90d554f811
TkachikNataly <tkachik.nataly@gmail.com>
2022-01-29 22:17:53 +0200

    push 2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+const galleryItemsList = makeGalleryItemsListMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryItemsList);
+
+function makeGalleryItemsListMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+      <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt= "${description}"
+      />
+      </a>
+      </li>    
+    `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
