cb7d9a5755e2bc20f1505ab3045907d87a26db6c
TkachikNataly <tkachik.nataly@gmail.com>
2022-02-06 00:26:16 +0200

    Finish pravki

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,8 +14,22 @@ function setCurrentVideoTime() {
     const saveCurrentTime = localStorage.getItem('videoplayer-current-time');
 
     if (saveCurrentTime) {
+       player.setCurrentTime(saveCurrentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
         player.setCurrentTime(saveCurrentTime)
     }
 }
+
 
 setCurrentVideoTime();
